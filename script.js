309d7d7c66fe7589778fc0876c7a29e1467c3d87
Mohamed Mansour <moyamed2mansour@gmail.com>
2024-04-02 00:54:46 +0300

    Creating function todo html

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ let todosJson = JSON.parse(localStorage.getItem("todos")) || [];
 const deleteAllButton = document.querySelector(".delete-all");
 const filters = document.querySelectorAll(".filter");
 let filter = '';
+
+showTodos();
+
+function getTodoHtml(todo, index) {
+    if (filter && filter != todo.status) {
+        return '';
+    }
+    let checked = todo.status == "completed" ? "checked" : "";
+    return /* html */ `
+        <li class="todo">
+        <label for="${index}">
+            <input id="${index}" onclick="updateStatus(this)" type="checkbox" ${checked}>
+            <span class="${checked}">${todo.name}</span>
+        </label>
+        <button class="delete-btn" data-index="${index}" onclick="remove(this)"><i class="fa fa-times"></i></button>
+        </li>
+    `; 
+}
